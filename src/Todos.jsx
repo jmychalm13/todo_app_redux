5c5bb0b7ae9a7fdcb051e695a1c7c32b4a677435
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-03-31 16:54:21 -0500

    map over todos variable

--- src/Todos.jsx
+++ src/Todos.jsx
@@ -1,7 +1,16 @@
+import { useSelector } from "react-redux";
+
 export function Todos() {
+  const todos = useSelector((state) => state.todos.todos);
   return (
     <div>
       <h1>All todos</h1>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <p>{todo.text}</p>
+          <small>{todo.status}</small>
+        </div>
+      ))}
     </div>
   );
 }
