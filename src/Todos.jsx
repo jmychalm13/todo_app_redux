a61053898cb94d0a9fa09b94432dd8393dde2352
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-04-01 07:37:22 -0500

    adds in the addToTodos function from the store

--- src/Todos.jsx
+++ src/Todos.jsx
@@ -4,15 +4,33 @@ import { addToTodos } from "./store/slice";
 import { useState } from "react";
 
 export function Todos() {
+  const dispatch = useDispatch();
   const todos = useSelector((state) => state.todos.todos);
-  const [todoText, setTodoText] = useState("Get a job");
+  const [todoText, setTodoText] = useState("");
+
+  const handleAdd = () => {
+    if (todoText === "") {
+      return;
+    }
+    // dispatches an action to add a todo using the store function
+    dispatch(
+      addToTodos({
+        id: Math.floor(Math.random() * 1000),
+        text: todoText,
+        status: "incomplete",
+      })
+    );
+
+    setTodoText("");
+  };
+
   return (
     <div>
       <h1>All Todos</h1>
       <div>
         <label>Add Todo:</label>
         <input value={todoText} onChange={(event) => setTodoText(event.target.value)} type="text" />
-        <button>Add</button>
+        <button onClick={handleAdd}>Add</button>
       </div>
       {todos.map((todo) => (
         <Todo key={todo.id} todo={todo}></Todo>
